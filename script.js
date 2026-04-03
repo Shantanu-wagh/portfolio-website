b4b9fe7140fb3e63c12dfbeae217f30d77e88b45
Shantanu Wagh <Shantanuwagh13@gmail.com>
2026-04-03 13:59:02 +0530

    Day 3 JS Added

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Welcome to my portfolio!");
+function showMessage() {
+  alert("Check out my projects below!");
+}
+function contactMe() {
+  alert("Email me at shantanuwagh13@gmail.com");
+}
